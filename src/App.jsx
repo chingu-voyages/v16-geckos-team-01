fc82baa790b5aeb599f82b8481d99400346ee1e5
Danielle Rubin <dlhrubin@gmail.com>
2020-02-13 16:10:29 -0500

    Configure ESLint and Sass

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="page-header">Trello Clone</h1>
+    </div>
+  );
+}
+
+export default App;
