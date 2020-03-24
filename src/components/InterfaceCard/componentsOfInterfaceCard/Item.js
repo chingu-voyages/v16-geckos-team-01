49aec82f1da0cfc2f11b56fd72ffae3c4ae8ef1c
upluke <lukeforadream@gmail.com>
2020-03-23 19:52:16 -0700

    innerscreen

--- src/components/InterfaceCard/componentsOfInterfaceCard/Item.js
+++ src/components/InterfaceCard/componentsOfInterfaceCard/Item.js
@@ -2,13 +2,28 @@ import React,{useState} from 'react'
 import Plane from './ItemDetail/Plane'
 
 export default ({cardName, id, titleName, members})=>{
-    // console.log("inside item", cardName,id)
+ 
     const [isPop, setIsPop] =useState(false)
-    console.log(isPop)
+   
+    const promptControl = isPop
+    && ( 
+        <div>
+            <div className="innerScreen"   />
+            <Plane 
+            cardName={cardName} 
+            titleName={titleName} 
+            isPop={isPop} 
+            setIsPop={setIsPop} 
+            members={members}
+            />
+        </div>
+    );
     return(
         <div className="item">
+         
              <div onClick={()=>setIsPop(true)}> {cardName}</div>
-             {isPop===true&&<Plane cardName={cardName} titleName={titleName} isPop={isPop} setIsPop={setIsPop} members={members}/>}
-        </div>
+             {promptControl}
+          </div>
+       
     )
 }
